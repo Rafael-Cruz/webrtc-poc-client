7c752fd3b84a05788aa5f871eb0f8453abecae8e
Rafael Cruz <rafael.cruz_dextra@prestador.globo>
2021-12-28 12:55:32 -0300

    Creates simple connection example page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useRef, useEffect } from 'react'
+import { io } from "socket.io-client";
 import './App.css';
 
+const SERVER_URL = 'http://teste.local:8080'
+
+const getMyMedia = () => {
+  const constraints = {
+    audio: false,
+    video: true
+  }
+  return navigator.mediaDevices.getUserMedia(constraints)
+}
+
+const createSocket = async () => {
+  const socket = io(SERVER_URL, {
+    rejectUnauthorized: false
+  })
+  
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+  socket.on("connect", () => {
+    console.log('conectado')
+  })
+}
+
+const createOrJoinRoom = async () => {
+  createSocket()
+}
+
+const setupMyMedia = async (myVideoElem) => {
+  try {
+    const stream = await getMyMedia()
+    myVideoElem.srcObject = stream
+  } catch(e) {
+    console.log('[setupMyMedia] Error: ', e)
+  }
+}
+
 function App() {
+  const myVideoRef = useRef(null)
+  const partnerVideoRef = useRef(null)
+  
+  const setupCall = async () => {
+    await createOrJoinRoom()
+    await setupMyMedia(myVideoRef.current)
+  }
+
+  useEffect(() => {
+    setupCall()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay ref={myVideoRef}></video>
+      <video autoPlay ref={partnerVideoRef}></video>
     </div>
   );
 }
