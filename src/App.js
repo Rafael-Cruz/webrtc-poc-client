920366d2dc105c6ea80dc38d94fc65578e99fe0b
Rafael Cruz <rafael.rblc@gmail.com>
2021-12-28 22:16:45 -0300

    Creates initial connection logic

--- src/App.js
+++ src/App.js
@@ -1,61 +1,44 @@
 import { useRef, useEffect } from 'react'
-import { io } from "socket.io-client";
 import './App.css';
 
-const SERVER_URL = 'http://teste.local:8080'
-
-const getMyMedia = () => {
-  const constraints = {
-    audio: false,
-    video: true
-  }
-  return navigator.mediaDevices.getUserMedia(constraints)
-}
-
-const createSocket = async () => {
-  const socket = io(SERVER_URL, {
-    rejectUnauthorized: false
-  })
-  
-  socket.on("connect_error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-  });
-
-  socket.on("connect", () => {
-    console.log('conectado')
-  })
-}
-
-const createOrJoinRoom = async () => {
-  createSocket()
-}
-
-const setupMyMedia = async (myVideoElem) => {
-  try {
-    const stream = await getMyMedia()
-    myVideoElem.srcObject = stream
-  } catch(e) {
-    console.log('[setupMyMedia] Error: ', e)
-  }
-}
+import {
+  createSocket,
+  createOrJoinRoom,
+  setupMyVideoElem,
+  onPartnerMediaStreamReceived
+} from './lib/connection'
 
 function App() {
+  const roomNameRef = useRef(null)
   const myVideoRef = useRef(null)
   const partnerVideoRef = useRef(null)
   
-  const setupCall = async () => {
-    await createOrJoinRoom()
-    await setupMyMedia(myVideoRef.current)
-  }
-
   useEffect(() => {
-    setupCall()
+    createSocket()
+    setupMyVideoElem(myVideoRef.current)
   }, [])
+
+  const handleJoinRoomClick = (e) => {
+    const roomName = roomNameRef.current.value
+    onPartnerMediaStreamReceived((stream) => {
+      console.log('onPartnerMediaStreamReceived called ', stream)
+      partnerVideoRef.current.srcObject = stream
+    })
+    createOrJoinRoom(roomName)
+  }
 
   return (
     <div className="App">
-      <video autoPlay ref={myVideoRef}></video>
-      <video autoPlay ref={partnerVideoRef}></video>
+      <div className="grid-item room-name-container">
+        <input type="text" ref={roomNameRef} />
+        <button onClick={handleJoinRoomClick}>
+          Join room
+        </button>
+      </div>
+      <div className="grid-item videos-container">
+        <video autoPlay ref={myVideoRef}></video>
+        <video autoPlay ref={partnerVideoRef}></video>
+      </div>
     </div>
   );
 }
---
+++ src/lib/connection.js
@@ -0,0 +1,224 @@
+import { connect, io } from "socket.io-client";
+
+const SERVER_URL = 'http://teste.local:8080'
+const iceServers = [
+  {
+    urls: 'stun:stun.l.google.com:19302'
+  },
+  {
+    urls: 'turn:numb.viagenie.ca',
+    credential: 'muazkh',
+    username: 'webrtc@live.com'
+  }
+]
+
+let socket
+
+let myMediaStream
+let partnerMediaStream
+let connection
+
+let roomName
+let isCaller
+
+let callbackTrackReceived
+
+const getSocket = () => {
+  if (socket === null) {
+    createSocket()
+  }
+
+  return socket
+}
+
+const getMyMedia = () => {
+  const constraints = {
+    audio: true,
+    video: true
+  }
+  return navigator.mediaDevices.getUserMedia(constraints)
+}
+
+const handleIceCandidate = ({ candidate }) => {
+  if (candidate) {
+    console.log('sending ice candidate', candidate)
+    
+    const {
+      sdpMLineIndex,
+      sdpMid,
+      usernameFragment,
+      candidate: innerCandidate
+    } = candidate
+
+    socket.emit('candidate', {
+      roomName,
+      sdpMLineIndex,
+      sdpMid,
+      usernameFragment,
+      candidate: innerCandidate
+    })
+  }
+}
+
+const handleAddStream = (e) => {
+  console.log('handling track', e)
+
+  partnerMediaStream = e.streams[0]
+  callbackTrackReceived(partnerMediaStream)
+}
+
+export const createSocket = () => {
+  socket = io(SERVER_URL, {
+    rejectUnauthorized: false
+  })
+
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+  socket.on("connect", () => {
+    console.log('conectado')
+  })
+
+  socket.on("created", (room) => {
+    roomName = room
+    isCaller = true
+    console.log(`room ${room} was created`)
+  })
+
+  socket.on("joined", (room) => {
+    roomName = room
+    isCaller = false
+    console.log(`joined ${room}`)
+  })
+
+  socket.on("full room", (roomName) => {
+    console.log(`${roomName} is full`)
+  })
+
+  socket.on("ready", () => {
+    console.log("room ready")
+
+    if (!isCaller) {
+      console.log('not the caller; ready event skipped')
+      return
+    }
+
+    connection = new RTCPeerConnection({ iceServers })
+    connection.onicecandidate = handleIceCandidate
+    connection.ontrack = handleAddStream
+
+    connection.onnegotiationneeded = async () => {
+      console.log('onnegotiationneeded event called')
+      try {
+        const offer = await connection.createOffer()
+        await connection.setLocalDescription(offer)
+
+        console.log('Offer created: ', offer)
+
+        socket.emit('offer', {
+          roomName,
+          sdp: offer
+        })
+
+      } catch (e) {
+        console.log('Error on offer creation: ', e)
+      }
+    }
+
+    const audioTracks = myMediaStream.getAudioTracks()
+    const videosTracks = myMediaStream.getVideoTracks()
+
+    connection.addTrack(audioTracks[0], myMediaStream)
+    connection.addTrack(videosTracks[0], myMediaStream)
+  })
+
+  socket.on('offer', async (sdp) => {
+    console.log('Offer received', sdp)
+
+    if (isCaller) {
+      console.log('is caller; skipping offer handling')
+      return
+    }
+
+    try {
+      connection = new RTCPeerConnection({ iceServers })
+      connection.onicecandidate = handleIceCandidate
+      connection.ontrack = handleAddStream
+
+      const audioTracks = myMediaStream.getAudioTracks()
+      const videosTracks = myMediaStream.getVideoTracks()
+
+      connection.addTrack(audioTracks[0], myMediaStream)
+      connection.addTrack(videosTracks[0], myMediaStream)
+
+      await connection.setRemoteDescription(sdp)
+
+      const answer = await connection.createAnswer()
+      await connection.setLocalDescription(answer)
+
+      console.log('Answer created: ', answer)
+
+      socket.emit('answer', {
+        roomName,
+        sdp: answer
+      })
+
+    } catch (e) {
+      console.log('error on offer handling: ', e)
+    }
+  })
+
+  socket.on('answer', (sdp) => {
+    console.log('answer received', sdp)
+
+    if (!isCaller) {
+      console.log('not the caller; answer event skipped')
+      return
+    }
+
+    connection.setRemoteDescription(sdp)
+  })
+
+  socket.on('candidate', ({ 
+    sdpMLineIndex,
+    sdpMid,
+    usernameFragment,
+    candidate
+  }) => {
+    console.log('receiving ice candidate', {
+      sdpMLineIndex,
+      usernameFragment,
+      sdpMid,
+      candidate
+    })
+
+    connection.addIceCandidate({
+      sdpMLineIndex,
+      sdpMid,
+      usernameFragment,
+      candidate
+    })
+  })
+}
+
+export const setupMyVideoElem = async (myVideoElem) => {
+  try {
+    const stream = await getMyMedia()
+    myMediaStream = stream
+
+    myVideoElem.muted = true
+    myVideoElem.srcObject = myMediaStream
+  } catch (e) {
+    console.log('[setupMyMedia] Error: ', e)
+  }
+}
+
+export const onPartnerMediaStreamReceived = (callback) => {
+  callbackTrackReceived = callback
+}
+
+export const createOrJoinRoom = (roomName) => {
+  const socket = getSocket()
+  socket.emit("create or join", roomName)
+}
