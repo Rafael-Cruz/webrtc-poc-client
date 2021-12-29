9ffef1d1e480b030c0472406068fb05b86425f7f
Rafael Cruz <rafael.rblc@gmail.com>
2021-12-29 14:25:53 -0300

    Adds simple chat handling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useState, useEffect, useCallback } from 'react'
 import './App.css';
 
 import {
   createSocket,
   createOrJoinRoom,
   setupMyVideoElem,
-  onPartnerMediaStreamReceived
+  onPartnerMediaStreamReceived,
+  sendMessage,
+  onReceiveMessage
 } from './lib/connection'
 
 function App() {
+  const [messages, setMessages] = useState([])
+  const messageBoxRef = useRef(null)
   const roomNameRef = useRef(null)
   const myVideoRef = useRef(null)
   const partnerVideoRef = useRef(null)
@@ -18,13 +22,38 @@ function App() {
     setupMyVideoElem(myVideoRef.current)
   }, [])
 
-  const handleJoinRoomClick = (e) => {
+  const handleJoinRoomClick = () => {
     const roomName = roomNameRef.current.value
+
     onPartnerMediaStreamReceived((stream) => {
       console.log('onPartnerMediaStreamReceived called ', stream)
       partnerVideoRef.current.srcObject = stream
     })
+
+    onReceiveMessage((msg) => {
+      setMessages(oldState => ([
+        ...oldState,
+        {
+          sender: 'partner',
+          msg
+        }
+      ]))
+    })
+
     createOrJoinRoom(roomName)
+  }
+
+  const handleSendMessageClick = () => {
+    const messageBox = messageBoxRef.current
+    const msgValue = messageBox.value
+    
+    sendMessage(msgValue)
+    messageBox.value = ''
+
+    setMessages([ ...messages, {
+      sender: 'me',
+      msg: msgValue
+    }])
   }
 
   return (
@@ -38,6 +67,27 @@ function App() {
       <div className="grid-item videos-container">
         <video autoPlay ref={myVideoRef}></video>
         <video autoPlay ref={partnerVideoRef}></video>
+      </div>
+      <div className="grid-item chat-container">
+        <div className="chat-messages">
+          {messages.map(({ sender, msg }) => {
+            const isMyMessage = sender === 'me'
+            return (
+              <p key={`${new Date().toDateString()}${sender}${msg}`} className="message-line">
+                <span className={`message-sender ${isMyMessage ? 'me' : 'partner'}`}>
+                  {isMyMessage ? 'me: ' : 'partner: '}
+                </span>
+                <span className='message-content'>
+                  {msg}
+                </span>
+              </p>
+            )
+          })}
+        </div>
+        <input type="text" ref={messageBoxRef} />
+        <button onClick={handleSendMessageClick}>
+          Send
+        </button>
       </div>
     </div>
   );
